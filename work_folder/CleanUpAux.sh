1709e66bc3b38891982f744d492264be4c95cb07
prateekrajgautam <prateekrajgautam@gmail.com>
2023-08-01 16:41:51 +0530

    INITIAL 2023

---
+++ work_folder/CleanUpAux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Clean Auxilary files from Latex Folder
+filetypes=(aux out log lof lot toc ind ilg idx glo bcf maf ist glsdefs mtc0 mtc gls glg 2i 2o blg run.xml bbl mtc* mlf* mlt* ptc* plf* plt* nav snm synctex.gz)
+for i in ${filetypes[@]}; do
+	rm ./*.$i
+done
+
+
---
+++ work_folder/compileAll.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+files=(ListOfPublicationsOnly PlainListOfPublicationsOnly Dr.PrateekRajGautam_Resume_2023_V01 Dr.PrateekRajGautam_Resume_2023_V01_schooling)
+rm PDF -rf
+mkdir PDF
+
+for f in ${files[@]}; do
+    echo Compiling $f
+    pdflatex "./"$f".tex"
+    biber ./$f
+    pdflatex ./$f".tex"
+    pdflatex ./$f".tex"
+    ./CleanUpAux.sh
+    mv "./"$f".pdf" "./PDF/"$f".pdf"
+
+done
+sudo apt install poppler-utils
+PDF="./Dr.PrateekRajGautam_Resume_2023_V01.pdf"
+echo "Converting "$PDF" to image"
+cd PDF
+pdftoppm -png -r 300 $PDF PNG
+cd ..
+echo "\n Command Completed"
