782a292af22595194bbb29da4bfe3fc8d4a55223
prateekrajgautam <prateekrajgautam@gmail.com>
2024-07-15 01:04:22 +0530

    q

--- work_folder/compileAll.sh
+++ work_folder/compileAll.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-files=(ListOfPublicationsOnly PlainListOfPublicationsOnly Dr.PrateekRajGautam_Resume_2024_V01 Dr.PrateekRajGautam_Resume_2024_V01_schooling)
+files="ListOfPublicationsOnly PlainListOfPublicationsOnly Dr.PrateekRajGautam_Resume_2024_V01 Dr.PrateekRajGautam_Resume_2024_V01_schooling"
+#files=(ListOfPublicationsOnly PlainListOfPublicationsOnly Dr.PrateekRajGautam_Resume_2024_V01 Dr.PrateekRajGautam_Resume_2024_V01_schooling)
 rm PDF -rf
 mkdir PDF
 
-for f in ${files[@]}; do
+for f in $files; do
+#for f in ${files[@]}; do
     echo Compiling $f
     pdflatex "./"$f".tex"
     biber ./$f
